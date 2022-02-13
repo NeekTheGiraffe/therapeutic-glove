87ac794019303292402ee5a1532e7542e98c9b44
Nick Pavlosky <44681231+NeekTheGiraffe@users.noreply.github.com>
2022-02-13 12:54:09 -0800

    Added basic 3-tab functionality

--- App.js
+++ App.js
@@ -1,12 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Home screen</Text>
+      <Button
+        title="Go to settings"
+        onPress={() => navigation.navigate("Settings")}
+      />
+    </View>
+  )
+}
+
+function GamesScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Game screen</Text>
+    </View>
+  )
+}
+
+function SettingsScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Settings screen</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            var iconName;
+
+            switch (route.name) {
+              case 'Home':
+                iconName = focused ? 'ios-stats-chart' : 'ios-stats-chart-outline';
+                break;
+              case 'Games':
+                iconName = focused ? 'md-game-controller' : 'md-game-controller-outline';
+                break;
+              case 'Settings':
+                iconName = focused ? 'md-list' : 'md-list-outline' ;
+            }
+
+            // Return icon component
+            return <Ionicons name={iconName} size={size} color={color} />
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen}/>
+        <Tab.Screen name="Games" component={GamesScreen}/>
+        <Tab.Screen name="Settings" component={SettingsScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
